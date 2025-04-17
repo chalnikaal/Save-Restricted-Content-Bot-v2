0023f07272953000064f7ebd9f42bf46362555d3
chalnikaal <chalnikaal90@gmail.com>
2025-04-17 18:23:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "22418298"))
-API_HASH = getenv("API_HASH", "5464393ff89900f4d07195e772bb5382")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5233308016").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://nawap51116:JUEM1GZSXlV6UUqp@cluster0.mdxx7s1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", ")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
